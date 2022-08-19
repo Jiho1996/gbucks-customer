18cb42a73df81fa92699671446b88d683e9f2d78
jiho1996 <jiho.urssu@gmail.com>
2022-08-19 17:10:50 +0900

    feat : 메뉴 전체 컴포넌트 생성

--- src/js/App.js
+++ src/js/App.js
@@ -8,19 +8,35 @@ export default class App extends Menu{
     async setup(){
         this.$state = this.$props;
         console.log(this.$state)
-        
     }
 
     template () {
         return `
         <header id="nav-container" data-component = "menu-info-bar"></header>
-        <ul id="slides_espresso" data-component = "menu-slider"></ul>
+        <ul id="slides_espresso" data-component = "esspresso-menu-slider"></ul>
+        <ul id="slides_espresso" data-component = "frappuccino-menu-slider"></ul>
+        <ul id="slides_espresso" data-component = "blended-menu-slider"></ul>
+        <ul id="slides_espresso" data-component = "teavana-menu-slider"></ul>
+        <ul id="slides_espresso" data-component = "desert-menu-slider"></ul>
         `
     }
 
     mount (){
-        const $menuSlider = $('[data-component="menu-slider"]');
-        new MenuSlider($menuSlider, this.$state);
+        const $esspressoMenuSlider = $('[data-component="esspresso-menu-slider"]');
+        new MenuSlider($esspressoMenuSlider, "espresso" ,this.$state);
+
+        const $frappuccinoMenuSlider = $('[data-component="frappuccino-menu-slider"]');
+        new MenuSlider($frappuccinoMenuSlider, "frappuccino" ,this.$state);
+
+        const $blendedMenuSlider = $('[data-component="blended-menu-slider"]');
+        new MenuSlider($blendedMenuSlider, "blended" ,this.$state);
+
+        const $teavanaMenuSlider = $('[data-component="teavana-menu-slider"]');
+        new MenuSlider($teavanaMenuSlider, "teavana" ,this.$state);
+        
+        const $desertMenuSlider = $('[data-component="desert-menu-slider"]');
+        new MenuSlider($desertMenuSlider, "desert" ,this.$state);
+
         const $topNavBar = $('[data-component="menu-info-bar"]');
         new TopNavBar($topNavBar);
         
