d6026c71bfc84fce0d6601f5f1afd1a56302715f
jiho1996 <jiho.urssu@gmail.com>
2022-08-19 16:31:33 +0900

    refactor : 메뉴 슬라이더 기초 틀 컴포넌트 분리

---
+++ src/js/components/MenuSlider.js
@@ -0,0 +1,22 @@
+import Menu from "../core/Menu.js"
+
+export default class MenuSlider extends Menu{
+    
+    async setup(){
+        //this.category = this.$props.category
+        this.$state = this.$props.espresso
+        console.log(this.$state);
+        
+    }
+
+    template () {
+        return this.$state.menu.map(ele => `<div>${ele.name}</div>`).join("");
+    }
+    
+    // const renderMenuList = async (menuCategory) =>{
+    //    this.menu[menuCategory] = await MenuApi.getAllMenuByCategory(menuCategory);
+    //    const template = this.menu[menuCategory]
+    //    .map((item) => {return `<div>${item.name}</div>`}).join("");
+    //    document.querySelector("#test").innerHTML = template;
+    // }
+    }
